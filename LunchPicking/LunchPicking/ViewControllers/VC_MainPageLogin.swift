65a43454ade109009a3388a3efb3e22ea38410ad
Lucas Luu <luuvanluc@gmail.com>
2020-08-17 22:22:44 +0700

    Initial project 2

---
+++ LunchPicking/LunchPicking/ViewControllers/VC_MainPageLogin.swift
@@ -0,0 +1,18 @@
+//
+//  VC_MainPageLogin.swift
+//  LunchPicking
+//
+//  Created by Luu Lucas on 8/17/20.
+//  Copyright © 2020 Luu Lucas. All rights reserved.
+//
+
+import UIKit
+
+class VC_MainPageLogin: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+}
